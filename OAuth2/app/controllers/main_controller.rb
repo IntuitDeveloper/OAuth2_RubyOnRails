82ff4a3bd2924c297feece37ee7d5260e442e34d
hlu2 <Hao_Lu@intuit.com>
2017-10-03 12:45:17 -0700

    Update the view

--- OAuth2/app/controllers/main_controller.rb
+++ OAuth2/app/controllers/main_controller.rb
@@ -3,6 +3,6 @@ require 'yaml'
 class MainController < ApplicationController
   def index
       config = YAML.load_file(Rails.root.join('config/config.yml'))
-      params[:url] = config["Settings"]["oauth_start_uri"]
+      params[:url] = config["Settings"]["host_uri"] + "token/"
   end
 end
--- OAuth2/app/controllers/token_controller.rb
+++ OAuth2/app/controllers/token_controller.rb
@@ -18,22 +18,34 @@ class TokenController < ApplicationController
       @code = params[:code]
       #record your ReamID to your DB
       @realmID = params[:realmId]
-      exchange_code_for_token
+      result = exchange_code_for_token
+      params[:refresh_token] = result["refresh_token"]
+      params[:expires_in] = result["expires_in"]
+      params[:x_refresh_token_expires_in] = result["x_refresh_token_expires_in"]
+      params[:access_token] = result["access_token"]
+      params[:host_uri] = @hostURL.to_s
     else
-      # State is changed Page
+      render html: '<div>Your State is not matched, consider it hacked.<div>'.html_safe
     end
   end
 
-  def exchange_code_for_token
-    #url = URI(@exchangeURL)
-    url = URI("https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer")
-    params = {
-      'code' => @code.to_s,
-      'grant_type' => @grant_type.to_s,
-      'redirect_uri' => @redirect_uri.to_s
+  def edit
+    result = refresh_token
+    params[:updated_refresh_token] = result["refresh_token"]
+    params[:updated_expires_in] = result["expires_in"]
+    params[:updated_x_refresh_token_expires_in] = result["x_refresh_token_expires_in"]
+    params[:updated_access_token] = result["access_token"]
+    params[:host_uri] = @hostURL.to_s
+  end
+
+  def refresh_token
+    load_config
+    url = URI(@exchangeURL)
+    queryparams = {
+      'grant_type' => @refresh_token_scope.to_s,
+      'refresh_token' => params[:id].to_s
     }
     header_value = "Basic " + Base64.strict_encode64(@client_id.to_s + ":" + @client_secret.to_s)
-    p header_value
     headers = {
       'Content-type' => "application/x-www-form-urlencoded",
       'Accept' => "application/json",
@@ -45,18 +57,46 @@ class TokenController < ApplicationController
     # TO debug, enable the below Line
     #http.set_debug_output($stdout)
     req = Net::HTTP::Post.new(url, headers)
-    req.set_form_data(params, "&")
+    req.set_form_data(queryparams, "&")
     response = http.request(req)
-    p response.body
+    hash_response = JSON.parse(response.body)
+    return hash_response
+  end
+
+  def exchange_code_for_token
+    url = URI(@exchangeURL)
+    queryparams = {
+      'code' => @code.to_s,
+      'grant_type' => @grant_type.to_s,
+      'redirect_uri' => @redirect_uri.to_s
+    }
+    header_value = "Basic " + Base64.strict_encode64(@client_id.to_s + ":" + @client_secret.to_s)
+    headers = {
+      'Content-type' => "application/x-www-form-urlencoded",
+      'Accept' => "application/json",
+      'Authorization' => header_value
+    }
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # TO debug, enable the below Line
+    #http.set_debug_output($stdout)
+    req = Net::HTTP::Post.new(url, headers)
+    req.set_form_data(queryparams, "&")
+    response = http.request(req)
+    hash_response = JSON.parse(response.body)
+    return hash_response
   end
 
   def load_config
     config = YAML.load_file(Rails.root.join('config/config.yml'))
+    @hostURL = config["Settings"]["host_uri"]
     @baseURL = config["Constant"]["baseURL"]
     @exchangeURL = config["Constant"]["tokenURL"]
     @client_id = config['OAuth2']['client_id']
     @client_secret = config['OAuth2']['client_secret']
     @scope = config["Constant"]["scope"]
+    @refresh_token_scope = config["Constant"]["resfresh_grant_type"]
     @redirect_uri = config["Settings"]["redirect_uri"]
     @state = config["Settings"]["state"]
     @response_type = config["Constant"]["response_type"]
